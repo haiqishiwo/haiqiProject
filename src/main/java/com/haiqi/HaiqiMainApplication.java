ea17246501cca07174d0d99eb074fc46596959c8
haiqishiwo <403288421@qq.com>
2019-02-23 00:37:14 +0800

    项目开始

---
+++ src/main/java/com/haiqi/HaiqiMainApplication.java
@@ -0,0 +1,13 @@
+package com.haiqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiqiMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaiqiMainApplication.class, args);
+    }
+
+}
